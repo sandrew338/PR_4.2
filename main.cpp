42725dc92fd540d188fa522b1fe00876ca0e42a4
sandrew338 <andrey.nazarkevych@gmail.com>
2023-10-04 21:36:33 +0300

    3 com

--- main.cpp
+++ main.cpp
@@ -6,46 +6,47 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 int main()
 {
-	double x, y;
-	cout << "x = "; cin >> x;
-	// спосіб 1: розгалуження в скороченій формі
-	y = x * x + 1;
-	if (x < 4)
-	{
-		y += 4 * pow(x, 7) - pow(x, 5) + pow(x, 3) - 2;
-	}
-	if (4 <= x && x < 7)
-	{
-		y += atan((abs(x) + 1) / 2) + 8.3 * x;
-	}
-	if (x >= 7)
-	{
-		y += abs(log(2 * x + exp(4 * x + 1)));
-	}
-	cout << endl;
-	cout << "1) y = " << y << endl;
+	double x, xp, xk, dx, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
 	// спосіб 2: розгалуження в повній формі
-	y = x * x + 1;
-	if (x < 4)
+	while (x < xk)
 	{
-		y += 4 * pow(x, 7) - pow(x, 5) + pow(x, 3) - 2;
-	}
-	else
-	{
-		if (x >= 7)
+		y = x * x + 1;
+		if (x < 4)
 		{
-			y += abs(log(2 * x + exp(4 * x + 1)));
+			y += 4 * pow(x, 7) - pow(x, 5) + pow(x, 3) - 2;
 		}
 		else
 		{
-			y += atan((abs(x) + 1) / 2) + 8.3 * x;
+			if (x >= 7)
+			{
+				y += abs(log(2 * x + exp(4 * x + 1)));
+			}
+			else
+			{
+				y += atan((abs(x) + 1) / 2) + 8.3 * x;
+			}
 		}
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
 	}
+	
 
-	cout << "2) y = " << y << endl;
+	cout << "---------------------------"<< endl;
 	return 0;
 }
